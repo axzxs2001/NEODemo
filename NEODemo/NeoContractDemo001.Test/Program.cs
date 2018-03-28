c44a85f798f592fe72aedc3e5dd1a1a17b0aeaaf
桂素伟 <axzxs2001@163.com>
2018-03-28 21:01:02 +0800

    NeoContractDemo001创建。

---
+++ NEODemo/NeoContractDemo001.Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using Neo.Cryptography;
+using Neo.VM;
+using System;
+using System.IO;
+
+namespace NeoContractDemo001.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new ExecutionEngine(null, Crypto.Default);
+            engine.LoadScript(File.ReadAllBytes(@"D:\Repos\NEODemo\NEODemo\NeoContractDemo001\bin\Debug\NeoContractDemo001.avm"));
+
+            //using (ScriptBuilder sb = new ScriptBuilder())
+            //{
+            //    sb.EmitPush(2); // 对应形参 c
+            //    sb.EmitPush(4); // 对应形参 b
+            //    sb.EmitPush(3); // 对应形参 a
+            //    engine.LoadScript(sb.ToArray());
+            //}
+
+            engine.Execute(); // 开始执行
+
+            var result = engine.EvaluationStack.Peek().GetBigInteger(); // 在这里设置返回值
+            Console.WriteLine($"执行结果 {result}");
+            Console.ReadLine();
+        }
+    }
+}
